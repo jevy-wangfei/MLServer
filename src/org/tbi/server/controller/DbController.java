599dcf19ea8dada6a108c853372ad98aab21d05e
Danyang Li <u5526912@anu.edu.au>
2015-08-05 22:22:59 +1000

    initial server

---
+++ src/org/tbi/server/controller/DbController.java
@@ -0,0 +1,44 @@
+package org.tbi.server.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.tbi.server.entity.User;
+import org.tbi.server.service.DbService;
+
+@RestController
+@RequestMapping("/users")
+public class DbController {
+	
+	@Autowired
+	private DbService dbService;
+	
+	@RequestMapping("/addition")
+	public void addUser(){
+		User user = new User();
+		user.setName("Hello World!");
+		dbService.addUser(user);
+	}
+	
+	@RequestMapping("/deletion")
+	public void deletedUser(){
+		String name = "Hello World!";
+		dbService.deleteUser(name);
+	}
+	
+	@RequestMapping("/modification")
+	public void modifyUser(){
+		User user = new User();
+		user.setComment("Modified");
+		dbService.modifyUser(user);		
+	}
+	
+	@RequestMapping("/query")
+	public List<User> queryUser(){
+		String name = "Hello World!";
+		return dbService.queryUser(name);
+	}
+
+}
---
+++ src/org/tbi/server/entity/User.java
@@ -0,0 +1,39 @@
+package org.tbi.server.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
+public class User {
+	@Id
+	private String id;
+	
+	private String name;
+	
+	private String comment;
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
