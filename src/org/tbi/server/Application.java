54d7f9dbb634b5af4bb7a9008ea6ea4e7c254c28
Fei Wang <u5575182@anu.edu.au>
2015-08-06 14:08:52 +1000

    Repair Springboot jar to war problems

--- src/org/tbi/server/Application.java
+++ src/org/tbi/server/Application.java
@@ -2,12 +2,18 @@ package org.tbi.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 //The @SpringBootApplication annotation is equivalent to using @Configuration, @EnableAutoConfiguration and @ComponentScan with their default attributes.
 @SpringBootApplication
-
-public class Application {
+public class Application extends SpringBootServletInitializer{
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 	public static void main(String[] args){
 		SpringApplication.run(Application.class, args);		
 	}
+	
 }
